365b1f0cca58f5b84d218395b578ae9bbf151950
Kushal Das <mail@kushaldas.in>
2020-06-26 19:42:48 +0530

    Adds recursive query feature

--- cmd/exdns/main.go
+++ cmd/exdns/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kushaldas/exampledns/pkg/dserver"
 )
 
 func main(){
-	fmt.Printf("Hello World!")
 	dserver.Listen();
 }
--- pkg/dserver/dserver.go
+++ pkg/dserver/dserver.go
@@ -16,7 +16,7 @@ func Listen() {
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m := new(dns.Msg)
 	m.SetReply(r)
-	fmt.Printf("%#v\n", r)
+	fmt.Printf("%#v\n\n", r)
 	if r.MsgHdr.Opcode == dns.OpcodeQuery {
 		if len(r.Question) > 0 {
 			qs := r.Question[0]
@@ -24,6 +24,13 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 				in, err := dns.NewRR("kushaldas.in. 14400 IN A 51.159.23.159")
 				if err == nil {
 					m.Answer = append(m.Answer, in)
+				}
+			} else {
+
+				in, err := dns.Exchange(r, "1.1.1.1:53")
+				if err == nil {
+					fmt.Printf("%#v\n\n", in)
+					m.Answer = append(m.Answer, in.Answer...)
 				}
 			}
 		}
