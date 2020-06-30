5c5933ff2d365bfceded3e08f4448b0e1e08d08b
Kushal Das <mail@kushaldas.in>
2020-06-30 09:46:49 +0530

    Removes random print calls

--- pkg/dserver/dserver.go
+++ pkg/dserver/dserver.go
@@ -31,7 +31,6 @@ func Listen(port *int, serveraddr, proxyaddr *string) {
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m := new(dns.Msg)
 	m.SetReply(r)
-	fmt.Printf("%#v\n\n", r)
 	if r.MsgHdr.Opcode == dns.OpcodeQuery {
 		if len(r.Question) > 0 {
 			dialer, _ := proxy.SOCKS5("tcp", proxyurl, nil, proxy.Direct)
@@ -48,7 +47,6 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 			}
 			resp, err := dnsConn.ReadMsg()
 			if err == nil {
-				fmt.Printf("%#v\n\n", resp)
 				m.Answer = append(m.Answer, resp.Answer...)
 				m.Ns = append(m.Ns, resp.Ns...)
 				m.Extra = append(m.Extra, resp.Extra...)
